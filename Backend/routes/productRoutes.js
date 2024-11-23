bc599d1f99fb288bcddaf7942f6e9cb16df7b48e
Sheenam7 <sheenamdubey78@gmail.com>
2024-11-23 23:33:34 +0530

    FlashCard Commit

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController'); // Ensure the path is correct
+const productController = require('../controllers/productController'); // Ensure this path is correct
 
 // Route to add a product
 router.post('/add-product', productController.addProduct);
+
+// Route to fetch all products
+router.get('/products', productController.getProducts);
 
 module.exports = router;
