31717f9c446b6134cc5c2048e621b6291d12012a
Sheenam7 <sheenamdubey78@gmail.com>
2024-11-27 15:17:48 +0530

    okk

--- src/components/Join-Room/flashCard.jsx
+++ src/components/Join-Room/flashCard.jsx
@@ -18,25 +18,20 @@ const FlashCard = ({ image, name, description, category, startingPrice, status, 
         <div className={styles.statusButtonContainer}>
           {/* Status */}
           <p
-            className={`${styles.flashCardStatus} ${
-              status === "Close" ? styles.close : styles.open
-            }`}
+            className={`${styles.flashCardStatus} ${status === "Close" ? styles.close : styles.open
+              }`}
           >
             {status}
           </p>
 
           {/* View Details Button */}
           <button
-            className="mt-4 bg-transparent border border-blue-500 py-2 px-4 text-lg rounded-lg hover:bg-[#4b2b8f] hover:text-white transition"
+            className="joinButton mt-4 bg-transparent border border-blue-500 py-2 px-4 text-lg rounded-lg hover:bg-[#4b2b8f] hover:text-white transition"
             aria-label={`View details about ${title}`}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
           >
             Join Now
           </button>
+
 
         </div>
       </div>
